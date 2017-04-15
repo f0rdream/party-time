ea6f4615c3a750703d04f87c2b51b160bb511713
lawiet019 <913589432@qq.com>
2017-04-15 16:01:42 +0800

    addsche

--- party-time/src/page/addSche.vue
+++ party-time/src/page/addSche.vue
@@ -15,7 +15,7 @@
             <mt-field label="系统账号" placeholder="请输入系统账号"></mt-field>
             <mt-field label="登录密码" placeholder="请输入登录密码"></mt-field>
             </section>
-            <section>
+            <section class="button_section">
             <mt-button type="default">登录</mt-button>
             </section>
           </mt-tab-container-item>
@@ -23,48 +23,28 @@
             <section>
               <mt-field label="名称" placeholder="请输入事件名称" v-model="affair.title"></mt-field>
               <mt-field label="地点" placeholder="请输入事件地点" v-model="affair.detail"></mt-field>
-              <a class="mint-cell mint-field"><!---->
-                <div class="mint-cell-left"></div>
-                <div class="mint-cell-wrapper">
-                  <div class="mint-cell-title">
-                    <!----> <span class="mint-cell-text">开始时间</span> <!---->
-                  </div>
-                  <div class="mint-cell-value">
-                    <input type="datetime-local" class="mint-field-core" v-model="affair.start_time">
-                    <div class="mint-field-clear" style="display: none;">
-                      <i class="mintui mintui-field-error"></i></div>
-                    <span class="mint-field-state is-default">
-                      <i class="mintui mintui-field-default"></i>
-                    </span>
-                    <div class="mint-field-other">
-                    </div>
-                  </div>
-                </div>
-                <div class="mint-cell-right"></div> <!---->
-              </a>
-              <a class="mint-cell mint-field"><!---->
-                <div class="mint-cell-left"></div>
-                <div class="mint-cell-wrapper">
-                  <div class="mint-cell-title">
-                    <!----> <span class="mint-cell-text">结束时间</span> <!---->
-                  </div>
-                  <div class="mint-cell-value">
-                    <input type="datetime-local" class="mint-field-core"  v-model="affair.end_time">
-                    <div class="mint-field-clear" style="display: none;">
-                      <i class="mintui mintui-field-error"></i></div>
-                    <span class="mint-field-state is-default">
-                      <i class="mintui mintui-field-default"></i>
-                    </span>
-                    <div class="mint-field-other">
-                    </div>
-                  </div>
-                </div>
-                <div class="mint-cell-right"></div> <!---->
-              </a>
+              <mt-field label="开始时间" placeholder="请输入开始时间" v-model="showStart" @click.native="showPickerStart"></mt-field>
+              <mt-field label="结束时间" placeholder="请输入结束时间" v-model="showEnd" @click.native="showPickerEnd"></mt-field>
             </section>
 
             <section class="button_section">
             <mt-button type="default" @click="submit">确认</mt-button>
+            </section>
+            <section>
+              <mt-datetime-picker
+                ref="pickerstart"
+                v-model="pickerstart"
+                type="datetime"
+                @comfirm="closePickerStart"
+              >
+              </mt-datetime-picker>
+                <mt-datetime-picker
+                  ref="pickerend"
+                  v-model="pickerend"
+                  type="datetime"
+                  @comfirm="closePickerEnd"
+                >
+                </mt-datetime-picker>
             </section>
           </mt-tab-container-item>
         </mt-tab-container>
@@ -78,6 +58,8 @@
   export default {
     data () {
       return {
+        pickerstart: new Date(),
+        pickerend: new Date(),
         selected: '1',
         affair: {
           title: '',
@@ -89,13 +71,49 @@
     },
     methods: {
       submit () {
-        this.affair.start_time = this.affair.start_time + ':00Z'
-        this.affair.end_time = this.affair.end_time + ':00Z'
-        this.$http.post('tasks/create', this.affair).then(res => {
+        this.affair.start_time = this.pickerstart.getFullYear() + '-' + ((this.pickerstart.getMonth() + 1) < 10 ? '0' +
+            (this.pickerstart.getMonth() + 1) : (this.pickerstart.getMonth() + 1)) +
+          '-' + (this.pickerstart.getDate() < 10 ? '0' + this.pickerstart.getDate() : this.pickerstart.getDate()) + 'T' +
+          (this.pickerstart.getHours() < 10 ? '0' + this.pickerstart.getHours() : this.pickerstart.getHours()) + ':' +
+          (this.pickerstart.getMinutes() < 10 ? '0' + this.pickerstart.getMinutes() : this.pickerstart.getMinutes()) + ':00Z'
+        this.affair.end_time = this.pickerend.getFullYear() + '-' + ((this.pickerend.getMonth() + 1) < 10 ? '0' +
+            (this.pickerend.getMonth() + 1) : (this.pickerend.getMonth() + 1)) +
+          '-' + (this.pickerend.getDate() < 10 ? '0' + this.pickerend.getDate() : this.pickerend.getDate()) + 'T' +
+          (this.pickerend.getHours() < 10 ? '0' + this.pickerend.getHours() : this.pickerend.getHours()) + ':' +
+          (this.pickerend.getMinutes() < 10 ? '0' + this.pickerend.getMinutes() : this.pickerend.getMinutes()) + ':00Z'
+        this.$http.post('/tasks/create', this.affair).then(res => {
           if (res.title) {
             Toast('你已经创建任务' + res.title)
           }
         })
+      },
+      showPickerStart () {
+        this.$refs.pickerstart.open()
+      },
+      showPickerEnd () {
+        this.$refs.pickerend.open()
+      },
+      closePickerStart () {
+        this.$refs.pickerstart.close()
+      },
+      closePickerEnd () {
+        this.$refs.pickerend.close()
+      }
+    },
+    computed: {
+      showStart: function () {
+        return this.pickerstart.getFullYear() + '-' + ((this.pickerstart.getMonth() + 1) < 10 ? '0' +
+            (this.pickerstart.getMonth() + 1) : (this.pickerstart.getMonth() + 1)) +
+          '-' + (this.pickerstart.getDate() < 10 ? '0' + this.pickerstart.getDate() : this.pickerstart.getDate()) + '-' +
+          (this.pickerstart.getHours() < 10 ? '0' + this.pickerstart.getHours() : this.pickerstart.getHours()) + '-' +
+          (this.pickerstart.getMinutes() < 10 ? '0' + this.pickerstart.getMinutes() : this.pickerstart.getMinutes())
+      },
+      showEnd: function () {
+        return this.pickerend.getFullYear() + '-' + ((this.pickerend.getMonth() + 1) < 10 ? '0' +
+            (this.pickerend.getMonth() + 1) : (this.pickerend.getMonth() + 1)) +
+          '-' + (this.pickerend.getDate() < 10 ? '0' + this.pickerend.getDate() : this.pickerend.getDate()) + '-' +
+          (this.pickerend.getHours() < 10 ? '0' + this.pickerend.getHours() : this.pickerend.getHours()) + '-' +
+          (this.pickerend.getMinutes() < 10 ? '0' + this.pickerend.getMinutes() : this.pickerend.getMinutes())
       }
     }
   }
